d1af24da087876afa67608496841a2a9366292a5
Ani-San <anirudhsanker@gmail.com>
2025-11-19 00:02:04 +0530

    Pushed project version 1

---
+++ app.py
@@ -0,0 +1,15 @@
+"""
+app.py
+
+Main application entry point for the Sign Language Recognition system.
+
+Authors : Anirudh Sanker  (2023UCP1844) 
+          Manas Dekivadia (2023UCP1843)
+          Om Sonar        (2023UCP1831)
+"""
+
+from slr import main
+
+# Run the main function of the Sign Language Recognition system
+if __name__ == "__main__":
+    main()
